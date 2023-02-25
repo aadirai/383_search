45d0113e1a1ecd2d1f35e65705d918124d8aec13
aadirai <71307411+aadirai@users.noreply.github.com>
2023-02-25 16:22:08 -0500

    Add files via upload

---
+++ scrap.py
@@ -0,0 +1,18 @@
+c = [(1,2), (2,3), (9,7)]
+
+print(c[1][0])
+print(c[1][1])
+
+
+"""
+position, foodGrid = state
+    min = 0
+    h_list = []
+    for food in foodGrid.asList():
+        xy1 = position
+        xy2 = food
+        print("start")
+        print(abs(xy1[0] - xy2[0]) + abs(xy1[1] - xy2[1]))
+        print("end")
+    return min
+    """
